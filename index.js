6fe752f687c6ca5cd6c63abb200a019cf2e303d2
Agent-grey <markkibuthu@gmail.com>
2024-11-21 04:26:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+
+// companyName defined as a const set as Scuber
+const companyName = 'Scuber'
+
+// mostProfitableNeighborhood is declared as equal to Chelsea:
+let mostProfitableNeighborhood = 'Chelsea'
+
+// companyCeo is declared as equal to Susan Smith
+let companyCeo = 'Susan Smith'
